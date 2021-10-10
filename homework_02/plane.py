def8b6c8852ebbbd504611b445ecc2e8c1827b2d
Popov Alexander <raeman.job@gmail.com>
2021-10-10 12:34:07 +0500

    homework fix 1

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -10,10 +10,14 @@ class Plane(Vehicle):
     max_cargo:int = 21000
     
     def __init__(self, weight: int, fuel: int, fuel_consumption: int, max_cargo:int) -> None:
+        self.__init_cargo = self.cargo
         self.max_cargo = max_cargo
         super().__init__(weight, fuel, fuel_consumption)
 
     def load_cargo(self, weigth: int):
         if (self.cargo + weigth) > self.max_cargo:
             raise CargoOverload
-        pass
+        self.cargo = self.cargo + weigth
+    
+    def remove_all_cargo(self):
+        self.cargo = self.__init_cargo
